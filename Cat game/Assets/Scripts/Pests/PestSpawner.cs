72dd165a70ca8e197a7fbc39798fceac853a260f
FlashSpike <ivanwdoesminecraft@gmail.com>
2023-12-07 16:20:40 +0800

    Commit

--- Cat game/Assets/Scripts/Pests/PestSpawner.cs
+++ Cat game/Assets/Scripts/Pests/PestSpawner.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class PestSpawner : MonoBehaviour
 {
+
+    public bool StopSpawning = false;
+    public float spawnTime;
+    public float spawnDelay;
+
     public GameObject mosquitoSpawn_1;
     public GameObject cockroachSpawn_1;
     public GameObject mosquitoSpawn_2;
@@ -10,15 +15,25 @@ public class PestSpawner : MonoBehaviour
 
     void Start()
     {
+        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+
         StartCoroutine(ActivateSpawns());
-/*        mosquitoSpawn_1.SetActive(true);
+        mosquitoSpawn_1.SetActive(true);
         cockroachSpawn_2.SetActive(true);
         mosquitoSpawn_2.SetActive(true);
-        cockroachSpawn_1.SetActive(true);*/
+        cockroachSpawn_1.SetActive(true);
+    }
+
+    public void SpawnObject()
+    {
+        Instantiate(ActivateSpawns(), transform.position,transform.rotation);
+
+        CancelInvoke("SpawnObject");
     }
 
     IEnumerator ActivateSpawns()
     {
+
         yield return new WaitForSeconds(10f); // Wait for 10 seconds
 
         // Activate Mosquito spawn_1 and Cockroach spawn_2
@@ -30,8 +45,7 @@ public class PestSpawner : MonoBehaviour
         // Activate Mosquito spawn_2 and Cockroach spawn_1
         mosquitoSpawn_2.SetActive(true);
         cockroachSpawn_1.SetActive(true);
-
     }
-
-
 }
+
+
