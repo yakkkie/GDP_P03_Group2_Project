fea1f32235ca39905333cd90350811d9d0757542
FlashSpike <ivanwdoesminecraft@gmail.com>
2023-12-13 00:49:44 +0800

    Pest Spawner Code done? maybe?

--- Cat game/Assets/Scripts/Pests/PestSpawner.cs
+++ Cat game/Assets/Scripts/Pests/PestSpawner.cs
@@ -9,31 +9,34 @@ public class PestSpawner : MonoBehaviour
     public GameObject cockroachSpawn_2;
 
     private bool bowlLeftOut;
-    private float timeLeftOut;
+    private Coroutine spawnCoroutine;
+
+    public float spawnTime = 1800f; // Time in seconds (30 minutes)
+    public float initialSpawnDelay = 10f; // Delay before pests start spawning
 
     void Start()
     {
         bowlLeftOut = false;
-        timeLeftOut = 0f;
+        spawnCoroutine = null;
+
+        // Deactivate pests
+        DeactivatePests();
     }
 
     void Update()
     {
-        if (bowlLeftOut)
+        // Check if the bowl has been left out
+        if (bowlLeftOut && spawnCoroutine == null)
         {
-            timeLeftOut += Time.deltaTime;
-
-            // Check if the bowl has been left out for 30 minutes
-            if (timeLeftOut >= 1800f) // 1800 seconds = 30 minutes
-            {
-                SpawnPests();
-                bowlLeftOut = false; // Reset the timer
-            }
+            spawnCoroutine = StartCoroutine(ActivateSpawns());
         }
     }
 
-    void SpawnPests()
+    IEnumerator ActivateSpawns()
     {
+        // Delay before pests start spawning
+        yield return new WaitForSeconds(initialSpawnDelay);
+
         // Activate Mosquito spawn_1 and Cockroach spawn_2
         mosquitoSpawn_1.SetActive(true);
         cockroachSpawn_2.SetActive(true);
@@ -41,11 +44,43 @@ public class PestSpawner : MonoBehaviour
         // Activate Mosquito spawn_2 and Cockroach spawn_1
         mosquitoSpawn_2.SetActive(true);
         cockroachSpawn_1.SetActive(true);
+
+        yield return new WaitForSeconds(spawnTime);
+
+        // Deactivate pests after spawning time
+        DeactivatePests();
+
+        // Reset the bowlLeftOut code to false
+        bowlLeftOut = false;
+
+        spawnCoroutine = null; // Reset the coroutine to null
+    }
+
+    // Deactivate all pests
+    void DeactivatePests()
+    {
+        mosquitoSpawn_1.SetActive(false);
+        cockroachSpawn_1.SetActive(false);
+        mosquitoSpawn_2.SetActive(false);
+        cockroachSpawn_2.SetActive(false);
     }
 
     // Call this method when the bowl is left out
     public void BowlLeftOut()
     {
         bowlLeftOut = true;
+    }
+
+    // Call this method to cancel the spawning if the bowl is no longer left out
+    public void BowlNotLeftOut()
+    {
+        bowlLeftOut = false;
+        if (spawnCoroutine != null)
+        {
+            StopCoroutine(spawnCoroutine);
+            spawnCoroutine = null;
+            // Deactivate pests if the coroutine is stopped early
+            DeactivatePests();
+        }
     }
 }
